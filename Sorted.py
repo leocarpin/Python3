377f53a06f299b76b2fe3b646f052f02f5dd0bd8
leocarpin <70786864+leocarpin@users.noreply.github.com>
2021-09-01 21:03:10 -0300

    Sorted function
    Personal lib

---
+++ Sorted.py
@@ -0,0 +1,13 @@
+'''
+Sort(); altera uma lista
+Sorted() = significado classificado
+Sempre retorna uma lista com os iteraveis organizados
+pode organizar crescente ou decrescenteo com a funcao reverse= True
+
+Sorted() =  Nao altera o dado inicial apenas organiza/classifica
+'''
+
+n = [1, 3, 0, 6, 7, 2, 123, 56, 278, 99]
+print(n)
+print(sorted(n))
+print(sorted(n, reverse=True))
